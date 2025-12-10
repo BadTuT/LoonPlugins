2cb136ee326b2ec30102db9b6ebadef221e5d720
Bad_TuT <57835900+BadTuT@users.noreply.github.com>
2025-12-10 11:18:34 +0800

    Update Notability.js to reflect active subscription

--- Notability/Notability.js
+++ Notability/Notability.js
@@ -1,36 +1,27 @@
-/*************************************
-项目名称：Notability
-下载地址：https://t.cn/A6Cgjtei
-脚本作者：chxm1023
-电报频道：https://t.me/chxm1023
-使用声明：⚠️仅供参考，🈲转载与售卖！
-*************************************/
-
-var chxm1023 = JSON.parse($response.body);
-
-chxm1023 = {
-   "data" : {
-     "processAppleReceipt" : {
-       "error" : 0,
-       "subscription" : {
-         "productId" : "com.gingerlabs.Notability.premium_subscription",
-         "originalTransactionId" : "570001184068302",
-         "tier" : "premium",
-         "refundedDate" : null,
-         "refundedReason" : null,
-         "isInBillingRetryPeriod" : false,
-         "expirationDate" : "2099-09-09T09:09:09.000Z",
-         "gracePeriodExpiresAt" : null,
-         "overDeviceLimit" : false,
-         "expirationIntent" : null,
-         "__typename" : "AppStoreSubscription",
-         "user" : null,
-         "status" : "canceled",
-         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
-       },
-       "__typename" : "SubscriptionResult"
+const response = {
+  "data": {
+    "processAppleReceipt": {
+      "__typename": "SubscriptionResult",
+      "error": 0,
+      "subscription": {
+        "__typename": "AppStoreSubscription",
+        "status": "active",
+        "originalPurchaseDate": "2024-09-19T09:27:35.000Z",
+        "originalTransactionId": "570001185968888",
+        "expirationDate": "9999-12-31T23:59:59.000Z",
+        "productId": "com.gingerlabs.Notability.premium_subscription",
+        "tier": "premium",
+        "refundedDate": null,
+        "refundedReason": null,
+        "isInBillingRetryPeriod": false,
+        "gracePeriodExpiresAt": null,
+        "expirationIntent": "CUSTOMER_CANCELLED",
+        "overDeviceLimit": false,
+        "user": null
+      },
+      "isClassic": false
     }
   }
 };
 
-$done({body : JSON.stringify(chxm1023)});
+$done({ body: JSON.stringify(response) });
